0efc21d06870c7e30221ca3935f09a2099e2fd4d
Taras Vandzhala <149932259+InlogicVangik@users.noreply.github.com>
2026-09-02 19:36:59 +0300

    fix folders

--- src/homeworks/task_2_3/main.cpp
+++ src/homeworks/task_2_3/main.cpp
@@ -1,16 +1,41 @@
-// Task 2_3
-// Run:      pio run -e task_2_3 -t upload
-// Monitor:  pio device monitor -b 115200
-//
-// This whole folder (src/homeworks/task_2_3/) is compiled as ONE program.
-// You can add more .cpp/.h files here; just keep a single setup()/loop().
-
 #include <Arduino.h>
 
-void setup() {
+#define LED_PIN1 2
+#define LED_PIN2 17
+#define LED_PIN3 18
+
+constexpr uint16_t BLINK_INTERVAL_MS1 = 200;
+constexpr uint16_t BLINK_INTERVAL_MS2 = 500;
+constexpr uint16_t BLINK_INTERVAL_MS3 = 1000;
+
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Task 2_3: TODO");
+  delay(100); // Wait for Serial to initialize
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN3, OUTPUT);
+  Serial.println("Demo 02: serial counter started");
 }
 
-void loop() {
+void loop()
+{
+  static unsigned long prev1 = 0;
+  static unsigned long prev2 = 0;
+  static unsigned long prev3 = 0;
+
+  unsigned long now = millis();
+
+  if (now - prev1 >= BLINK_INTERVAL_MS1) {
+    prev1 = now;
+    digitalWrite(LED_PIN1, !digitalRead(LED_PIN1));
+  }
+  if (now - prev2 >= BLINK_INTERVAL_MS2) {
+    prev2 = now;
+    digitalWrite(LED_PIN2, !digitalRead(LED_PIN2));
+  }
+  if (now - prev3 >= BLINK_INTERVAL_MS3) {
+    prev3 = now;
+    digitalWrite(LED_PIN3, !digitalRead(LED_PIN3));
+  }
 }
